7098b206494c9f0fd57cfc96a7a27bd356581787
Natalia Soledad Gutierrez <nataliasolegutierrez@gmail.com>
2026-02-18 16:58:06 +0100

    Add cy.intercep to validate to login request status

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -17,9 +17,23 @@ describe('Login Flow - OrangeHRM', () => {
 
     it('should login with invalid password', () => {
 
+        cy.intercept('POST', '**/auth/**').as('loginRequest')
+
         LoginPage.enterUsername('Admin')
         LoginPage.enterPassword('Naty1234')
         LoginPage.clickLogin()
+
+        /*cy.wait('@loginRequest').its('response.statusCode').should('eq', 302)
+
+        cy.wait('@loginRequest').then((interception) => {
+            console.log(interception)
+          })*/
+        cy.wait('@loginRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(302)
+            console.log(interception)
+        })
+              
+          
 
         cy.contains('Invalid credentials').should('be.visible')
     })
